9c23d5fee248c17da91427baacc71e5a5f7803ba
EC2 Default User <ec2-user@ip-172-31-9-19.ap-northeast-1.compute.internal>
2023-03-01 09:41:47 +0000

    [Update]4-8

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_28_090257) do
+ActiveRecord::Schema.define(version: 2023_03_01_093214) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -45,6 +45,20 @@ ActiveRecord::Schema.define(version: 2023_02_28_090257) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "title"
     t.string "body"
+  end
+
+  create_table "post_images", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "shop_name"
+    t.text "caption"
+  end
+
+  create_table "postimages", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "shop_name"
+    t.text "caption"
   end
 
   create_table "users", force: :cascade do |t|
